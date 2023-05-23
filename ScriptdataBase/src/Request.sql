73806e356764c5753635905b0d36f969f841ddae
Maxime Pagé <maxime.page@usherbrooke.ca>
2023-05-23 11:38:25 -0400

    Auto stash before merge of "main" and "origin/main"

--- ScriptdataBase/src/Request.sql
+++ ScriptdataBase/src/Request.sql
@@ -3,5 +3,10 @@ AS
 BEGIN
     SELECT * FROM app2.Reservation
 
+CREATE OR REPLACE PROCEDURE example_procedure(param1 INT, param2 VARCHAR(255))
+AS
+BEGIN
+-- Instructions à exécuter dans la procédure
+-- Utilisez les paramètres passés à la procédure comme besoin
 
 --execute SelectReserv ('2019-01-01', '2019-01-31',01)
