5eb1f31b77f3aa45bbd705b0fbaa0c86f21592a6
Maxime Pagé <maxime.page@usherbrooke.ca>
2023-05-23 11:53:12 -0400

    bidon

--- ScriptdataBase/src/Request.sql
+++ ScriptdataBase/src/Request.sql
@@ -1,12 +1,10 @@
-CREATE PROCEDURE SelectReserv (date_debut_s timestamp, Date_fin_s timestamp, type varchar(30))
-AS
-BEGIN
-    SELECT * FROM app2.Reservation
+select * from app2.a_carac;
 
-CREATE OR REPLACE PROCEDURE example_procedure(param1 INT, param2 VARCHAR(255))
-AS
+CREATE OR REPLACE PROCEDURE SelectReserv (IN date_debut_s TIMESTAMP, IN date_fin_s TIMESTAMP, IN type VARCHAR(30))
+    LANGUAGE plpgsql
+AS $$
 BEGIN
--- Instructions à exécuter dans la procédure
--- Utilisez les paramètres passés à la procédure comme besoin
+    SELECT * FROM app2.Reservation;
+END;
+$$;
 
---execute SelectReserv ('2019-01-01', '2019-01-31',01)
